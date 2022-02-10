18798d9b045562527a9c0a2fc598561b22558fb4
Oliunina <lsbaranova1@gmail.com>
2022-02-10 17:16:56 -0500

    открыли калькулятор в гугле

---
+++ src/test/java/testsPractica/Calculat.java
@@ -0,0 +1,20 @@
+package testsPractica;
+
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Calculat {
+
+    @Test
+    public void test1() {
+      System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("калькулятор", Keys.ENTER);
+        driver.quit();
+    }
+}
