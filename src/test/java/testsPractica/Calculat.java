3b34fdc77fc6d81189fb6dbb3d2582265e8b4446
Oliunina <lsbaranova1@gmail.com>
2022-02-11 13:42:52 -0500

    предусловие и постусловие выведены

--- src/test/java/testsPractica/Calculat.java
+++ src/test/java/testsPractica/Calculat.java
@@ -1,6 +1,8 @@
 package testsPractica;
 
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -9,25 +11,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Calculat {
 
-    @Test
-    public void test1() {
-      System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://google.com");
-        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("калькулятор", Keys.ENTER);
-        driver.findElement(By.cssSelector("div [jsname=j93WEe]")).click();
-        driver.findElement(By.cssSelector("div [jsname=N10B9]")).click();
-        driver.findElement(By.cssSelector("div [jsname=XSr6wc]")).click();
-        driver.findElement(By.cssSelector("div [jsname=lVjWed]")).click();
-        driver.findElement(By.cssSelector("div [jsname=qCp9A]")).click();
-        driver.findElement(By.cssSelector("div [jsname=YovRWb]")).click();
-        driver.findElement(By.cssSelector("div [jsname=KN1kY]")).click();
-        driver.findElement(By.cssSelector("div [jsname=pPHzQc]")).click();
-        driver.findElement(By.cssSelector("div [jsname=xAP7E]")).click();
-        driver.findElement(By.cssSelector("div [jsname=bkEvMb]")).click();
-        driver.findElement(By.cssSelector("div [jsname=WxTTNd]")).click();
-        driver.findElement(By.cssSelector("div [jsname=Ax5wH]")).click();
-        driver.findElement(By.cssSelector("div [jsname=Pt8tGc]")).click();
-        driver.quit();
-    }
+  private static WebDriver driver;
+
+  @BeforeAll
+  public static void init() {
+    System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+    driver = new ChromeDriver();
+  }
+
+  @Test
+  public void test1() {
+    driver.get("http://google.com");
+    driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("калькулятор", Keys.ENTER);
+    driver.findElement(By.cssSelector("div [jsname=j93WEe]")).click();
+    driver.findElement(By.cssSelector("div [jsname=N10B9]")).click();
+    driver.findElement(By.cssSelector("div [jsname=XSr6wc]")).click();
+    driver.findElement(By.cssSelector("div [jsname=lVjWed]")).click();
+    driver.findElement(By.cssSelector("div [jsname=qCp9A]")).click();
+    driver.findElement(By.cssSelector("div [jsname=YovRWb]")).click();
+    driver.findElement(By.cssSelector("div [jsname=KN1kY]")).click();
+    driver.findElement(By.cssSelector("div [jsname=pPHzQc]")).click();
+    driver.findElement(By.cssSelector("div [jsname=xAP7E]")).click();
+    driver.findElement(By.cssSelector("div [jsname=bkEvMb]")).click();
+    driver.findElement(By.cssSelector("div [jsname=WxTTNd]")).click();
+    driver.findElement(By.cssSelector("div [jsname=Ax5wH]")).click();
+    driver.findElement(By.cssSelector("div [jsname=Pt8tGc]")).click();
+  }
+
+  @AfterAll
+  public static void feardown() {
+    driver.quit();
 }
+
